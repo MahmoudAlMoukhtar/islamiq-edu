b1775650d32ea1267546877fb3806d73366b488e
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-03-08 21:21:24 +0200

    remove cart page

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,6 @@ export default function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchAllProductsAction());
-    dispatch(fetchAllProductsCartAction());
   }, []);
 
   if (loading) return <Spinner />;
