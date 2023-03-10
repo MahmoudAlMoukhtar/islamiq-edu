7118b1e906b89a460272c970682a653f4ae05457
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-03-10 18:52:14 +0200

    edit slider

--- client/src/pages/Home/HeroSection/HeroSection.jsx
+++ client/src/pages/Home/HeroSection/HeroSection.jsx
@@ -30,89 +30,87 @@ const SlidePrevButton = () => {
 const HeroSection = () => {
   const [t, i18n] = useTranslation();
   return (
-    <div className="flex w-full">
-      <div className=" flex justify-center text-black  w-full">
-        <Swiper
-          modules={[Autoplay, Navigation]}
-          spaceBetween={0}
-          slidesPerView={1}
-          centeredSlides={true}
-          initialSlide={0}
-          loop={true}
-          navigation
-          autoplay={{delay: 3000}}
-          className="cursor-grap"
-        >
-          <SwiperSlide className="max-h-screen">
-            <LazyLoadImage
-              effect="blur"
-              loading="lazy"
-              src="https://res.cloudinary.com/dihhlmkrf/image/upload/v1678308216/islamiq/top-view-islamic-new-year-concept-with-copy-space_skc7bz.jpg"
-              className="w-full relative object-cover	h-[390px] sm:h-full"
-              alt="heroimage"
-            />
-            <div className="flex flex-col items-center justify-center text-center gap-6 w-full absolute top-[50%] left-[50%] sm:px-20 translate-x-[-50%] translate-y-[-50%] sm:translate-y-[-80%] md:translate-y-[-100%]">
-              <SlidePrevButton />
-              <p className="text-4xl md:text-5xl lg:text-6xl font-bold">
-                {t("mainSlider.1.title")}
-              </p>
-              <p className="font-semibold">{t("mainSlider.1.desciption")}</p>
-              <a
-                href="#fees"
-                className="py-2 px-8 rounded bg-[#4caf50] text-white font-bold text-black"
-              >
-                {t("mainSlider.1.button")}
-              </a>
-              <SlideNextButton />
-            </div>
-          </SwiperSlide>
+    <div className=" flex justify-center text-black  w-full sm:max-h-[400px] md:max-h-[500px] lg:max-h-[600px] overflow-y-hidden">
+      <Swiper
+        modules={[Autoplay, Navigation]}
+        spaceBetween={0}
+        slidesPerView={1}
+        centeredSlides={true}
+        initialSlide={0}
+        loop={true}
+        navigation
+        autoplay={{delay: 3000}}
+        className="cursor-grap"
+      >
+        <SwiperSlide className="">
+          <LazyLoadImage
+            effect="blur"
+            loading="lazy"
+            src="https://res.cloudinary.com/dihhlmkrf/image/upload/v1678308216/islamiq/top-view-islamic-new-year-concept-with-copy-space_skc7bz.jpg"
+            className="w-full relative object-cover	h-[390px] sm:h-full"
+            alt="heroimage"
+          />
+          <div className="flex flex-col items-center justify-center text-center gap-6 w-full absolute top-[50%] left-[50%] sm:px-20 translate-x-[-50%] translate-y-[-50%] sm:translate-y-[-80%] lg:translate-y-[-100%]">
+            <SlidePrevButton />
+            <p className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
+              {t("mainSlider.1.title")}
+            </p>
+            <p className="font-semibold">{t("mainSlider.1.desciption")}</p>
+            <a
+              href="#fees"
+              className="py-2 px-8 rounded bg-[#4caf50] text-white font-bold text-black"
+            >
+              {t("mainSlider.1.button")}
+            </a>
+            <SlideNextButton />
+          </div>
+        </SwiperSlide>
 
-          <SwiperSlide className="max-h-screen">
-            <LazyLoadImage
-              effect="blur"
-              loading="lazy"
-              src="https://res.cloudinary.com/dihhlmkrf/image/upload/v1678307907/islamiq/quran2_reos1a.jpg"
-              className="w-full relative object-cover	h-[390px] sm:h-full"
-            />
-            <div className="flex flex-col items-center justify-center text-center gap-6 w-full absolute top-[50%] left-[50%] sm:px-20 translate-x-[-50%] translate-y-[-50%] sm:translate-y-[-80%] md:translate-y-[-100%]">
-              <SlidePrevButton />
-              <p className="text-4xl md:text-5xl lg:text-6xl font-bold text-black">
-                {t("mainSlider.3.title")}
-              </p>
-              <p className="font-semibold">{t("mainSlider.3.desciption")}</p>
-              <Link
-                to="/auth"
-                className="py-2 px-8 rounded bg-[#4caf50] text-white font-bold text-black"
-              >
-                {t("mainSlider.3.button")}
-              </Link>
-              <SlideNextButton />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="max-h-screen">
-            <LazyLoadImage
-              effect="blur"
-              loading="lazy"
-              src="https://res.cloudinary.com/dihhlmkrf/image/upload/v1678308674/islamiq/vecteezy_verses-of-the-holy-quran_6034228_263_anazif.jpg"
-              className="w-full relative object-cover h-[390px]	sm:h-full"
-            />
-            <div className="flex flex-col items-center justify-center text-center gap-6 w-full absolute top-[50%] left-[50%] sm:px-20 translate-x-[-50%] translate-y-[-50%] sm:translate-y-[-80%] md:translate-y-[-100%]">
-              <SlidePrevButton />
-              <p className="text-4xl md:text-5xl lg:text-6xl font-bold text-black">
-                {t("mainSlider.2.title")}
-              </p>
-              <p className="font-semibold">{t("mainSlider.2.desciption")}</p>
-              <a
-                href="#courses"
-                className="py-2 px-8 rounded bg-[#4caf50] text-white font-bold text-black"
-              >
-                {t("mainSlider.2.button")}
-              </a>
-              <SlideNextButton />
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
+        <SwiperSlide className="">
+          <LazyLoadImage
+            effect="blur"
+            loading="lazy"
+            src="https://res.cloudinary.com/dihhlmkrf/image/upload/v1678307907/islamiq/quran2_reos1a.jpg"
+            className="w-full relative object-cover	h-[390px] sm:h-full"
+          />
+          <div className="flex flex-col items-center justify-center text-center gap-6 w-full absolute top-[50%] left-[50%] sm:px-20 translate-x-[-50%] translate-y-[-50%] sm:translate-y-[-80%] lg:translate-y-[-100%]">
+            <SlidePrevButton />
+            <p className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-black">
+              {t("mainSlider.3.title")}
+            </p>
+            <p className="font-semibold">{t("mainSlider.3.desciption")}</p>
+            <Link
+              to="/auth"
+              className="py-2 px-8 rounded bg-[#4caf50] text-white font-bold text-black"
+            >
+              {t("mainSlider.3.button")}
+            </Link>
+            <SlideNextButton />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <LazyLoadImage
+            effect="blur"
+            loading="lazy"
+            src="https://res.cloudinary.com/dihhlmkrf/image/upload/v1678308674/islamiq/vecteezy_verses-of-the-holy-quran_6034228_263_anazif.jpg"
+            className="w-full relative object-cover h-[390px] sm:h-full"
+          />
+          <div className="flex flex-col items-center justify-center text-center gap-6 w-full absolute top-[50%] left-[50%] sm:px-20 translate-x-[-50%] translate-y-[-50%] sm:translate-y-[-80%] lg:translate-y-[-100%]">
+            <SlidePrevButton />
+            <p className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-black">
+              {t("mainSlider.2.title")}
+            </p>
+            <p className="font-semibold">{t("mainSlider.2.desciption")}</p>
+            <a
+              href="#courses"
+              className="py-2 px-8 rounded bg-[#4caf50] text-white font-bold text-black"
+            >
+              {t("mainSlider.2.button")}
+            </a>
+            <SlideNextButton />
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
