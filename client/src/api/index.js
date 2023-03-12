63956a77db31b98a8e2b85734e7d724e73fb381e
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-03-12 17:43:14 +0200

    linke and blogs with dashboard

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api",
   //baseURL: "http://localhost:3001/api",
+  baseURL: "https://islamiq-edu-d114.vercel.app/api",
 });
 API.interceptors.request.use(req => {
   if (localStorage.getItem("userIqraa")) {
