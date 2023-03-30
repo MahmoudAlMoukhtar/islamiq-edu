78ee0c60511de304c1ed18122fe87c6a572a81aa
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-03-30 10:26:41 +0200

    test222333

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 // const coursesRoutes = require("./routes/courses");
 // const blogRoutes = require("./routes/blogs");
 // const userRoutes = require("./routes/users");
@@ -27,19 +27,22 @@ app.use(cors());
 // app.use("/api/comments", commentRouter);
 
 const PORT = process.env.PORT || 3001;
-mongoose
-  .connect(process.env.MONGODB_CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("success connection database");
+   app.listen(PORT, () => {
+     console.log(`success listning on ${PORT}`);
+   });
+// mongoose
+//   .connect(process.env.MONGODB_CONNECTION, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("success connection database");
 
-    app.listen(PORT, () => {
-      console.log(`success listning on ${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.log("error connection database!!!");
-    console.log(err.message);
-  });
+//     app.listen(PORT, () => {
+//       console.log(`success listning on ${PORT}`);
+//     });
+//   })
+//   .catch(err => {
+//     console.log("error connection database!!!");
+//     console.log(err.message);
+//   });
