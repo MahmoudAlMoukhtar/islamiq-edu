a01d8ef8463690ea2463ab6b14b1094d047b2b07
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-03-30 10:23:40 +0200

    testtttttt

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const blogRoutes = require("./routes/blogs");
-const userRoutes = require("./routes/users");
-const TestimoialsRouter = require("./routes/testimoials");
-const contactMessagesRouter = require("./routes/contactMessage");
-const newsRouter = require("./routes/newslitter");
-const commentRouter = require("./routes/comments");
-const compression = require("compression");
+// const coursesRoutes = require("./routes/courses");
+// const blogRoutes = require("./routes/blogs");
+// const userRoutes = require("./routes/users");
+// const TestimoialsRouter = require("./routes/testimoials");
+// const contactMessagesRouter = require("./routes/contactMessage");
+// const newsRouter = require("./routes/newslitter");
+// const commentRouter = require("./routes/comments");
+// const compression = require("compression");
 const app = express();
 
 app.use(bodyParser.json({extended: true}));
@@ -17,12 +18,13 @@ app.use(compression());
 
 app.use(cors());
 
-app.use("/api/blogs", blogRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/newsLetter", newsRouter);
-app.use("/api/testimoials", TestimoialsRouter);
-app.use("/api/contactMessages", contactMessagesRouter);
-app.use("/api/comments", commentRouter);
+// app.use("/api/blogs", blogRoutes);
+// app.use("/api/user", userRoutes);
+// app.use("/api/courses", coursesRoutes);
+// app.use("/api/newsLetter", newsRouter);
+// app.use("/api/testimoials", TestimoialsRouter);
+// app.use("/api/contactMessages", contactMessagesRouter);
+// app.use("/api/comments", commentRouter);
 
 const PORT = process.env.PORT || 3001;
 mongoose
